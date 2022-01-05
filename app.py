2a18c3764df5d8ec7022bb887e038229f9fe97bc
Lucky <175917645@qq.com>
2022-01-05 17:26:37 +0800

    add clip image

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask
 from flask_restful import Api
 from routes import init_routes
-from database import config
-from database.exts import db
+from lib.server.database import config
+# from database.exts import db
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
-app.config.from_envvar("ENV_FILE_LOCATION")
+# app.config.from_envvar("ENV_FILE_LOCATION")
 
 api = Api(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
 app.config.from_object(config)
-db.init_app(app)
+# db.init_app(app)
 
 init_routes(api)
 app.run()
--- routes.py
+++ routes.py
@@ -1,7 +1,9 @@
-from api.user import UsersApi
-from api.auth import SignupApi, LoginApi
+from lib.server.api.user import UsersApi
+from lib.server.api.auth import SignupApi, LoginApi
+from lib.server.api.inference import InferenceApi
 
 def init_routes(api):
 	api.add_resource(UsersApi, "/api/users")
 	api.add_resource(SignupApi, "/api/auth/signup")
 	api.add_resource(LoginApi, "/api/auth/login")
+	api.add_resource(InferenceApi, "/api/inference")
